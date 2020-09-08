368e160fe8955e1f22017370047de43ac84361d3
Shreevatsa M Sharma <54937921+shreevatsams123@users.noreply.github.com>
2020-09-08 14:56:14 +0530

    Add files via upload

---
+++ age/age.js
@@ -0,0 +1,25 @@
+
+const age = prompt("your age")
+if(age<16) {
+    console.log("wont you think of childern")
+}else if(age>=16 && age<18) {
+    console.log("not eleigible")
+}else if(age>=18 && age<21) {
+    console.log("no drinking")
+}else if(age>=21) {
+    console.log("get smashed")
+} else {
+  console.log("enter valid no");
+
+}
+
+
+if(age==21)
+{
+  console.log("happy 21 birthsay");
+} else if("age%2 != 0")
+{
+  console.log("you are odd");
+} else {
+  console.log("you ar perfect");
+}
